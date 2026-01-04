544f9a1841906cbcecf2b5031acd1476fe519f12
rathodss <rathodss991@gmail.com>
2026-01-04 11:40:59 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+public class whileloop {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i < 5) {
+            System.out.println("Hi " + i);
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("Hello " + j);
+                j++;
+            }
+            i++;
+        }
+        System.out.println("Bye " + i);
+
+    }
+
+}
